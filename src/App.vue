fa2303c0c628ad59f7aa626ab87b58b2e70ea058
huangzx <48216476+Hzhxi9@users.noreply.github.com>
2021-11-04 09:53:37 +0800

    vite: use css Module

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
+import style from './assets/css/message.module.css';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+
+// use css modules
+import { computed } from '@vue/reactivity';
+
+const $style = computed(() => style);
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <p :class="$style.message">hello</p>
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
 </template>
 
 <style>
